6929010dfba08ca32e15168bdfae3e08de635519
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-02-24 12:05:59 +0100

    #1 Fix parenthesis

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE expenses (
-  date TEXT NOT NULL CHECK (date(date) IS NOT NULL DEFAULT (date('now'))),
+  date TEXT NOT NULL CHECK (date(date) IS NOT NULL) DEFAULT (date('now')),
   amount DECIMAL(10,2) NOT NULL,
   description TEXT NOT NULL,
   tags TEXT NOT NULL CHECK (json_valid(tags) AND json_type(tags) = 'array') DEFAULT '[]');
