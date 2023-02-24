3884f823a1816c2e55e711576a587a6ea369cde6
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-02-24 12:01:32 +0100

    #1 Add missing ')'

--- schema.sql
+++ schema.sql
@@ -2,4 +2,4 @@ CREATE TABLE expenses (
   date TEXT NOT NULL CHECK (date(bar) IS NOT NULL),
   amount DECIMAL(10,2) NOT NULL,
   description TEXT NOT NULL,
-  tags TEXT NOT NULL CHECK (json_valid(tags) AND json_type(tags) = 'array');
+  tags TEXT NOT NULL CHECK (json_valid(tags) AND json_type(tags) = 'array'));
