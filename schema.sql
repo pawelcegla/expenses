479d0c35e72ed3776033530d3f029709a1fc4341
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-02-24 11:58:33 +0100

    #1 Add column constraints for date and tags validation

--- schema.sql
+++ schema.sql
@@ -1,1 +1,5 @@
-CREATE TABLE expenses (date TEXT NOT NULL, amount DECIMAL(10,2) NOT NULL, description TEXT NOT NULL, tags TEXT NOT NULL);
+CREATE TABLE expenses (
+  date TEXT NOT NULL CHECK (date(bar) IS NOT NULL),
+  amount DECIMAL(10,2) NOT NULL,
+  description TEXT NOT NULL,
+  tags TEXT NOT NULL CHECK (json_valid(tags) AND json_type(tags) = 'array');
