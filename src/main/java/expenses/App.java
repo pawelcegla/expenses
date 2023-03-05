76366d1e468c9e45915b2c3b9cbdbcb0d5d76d1c
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-03-05 20:12:01 +0100

    #7 Extend the basic test to use SQLite

--- src/main/java/expenses/App.java
+++ src/main/java/expenses/App.java
@@ -2,6 +2,7 @@ package expenses;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Hello world!
@@ -9,13 +10,16 @@ import java.sql.SQLException;
 public class App {
 
     public static void main(String[] args) throws SQLException {
-
         try (var c = DriverManager.getConnection("jdbc:sqlite::memory:");
-             var s = c.createStatement();
-             var rs = s.executeQuery("SELECT sqlite_version();")) {
+             var s = c.createStatement()) {
+            System.out.println(sqliteVersion(s));
+        }
+    }
 
+    static String sqliteVersion(Statement s) throws SQLException {
+        try (var rs = s.executeQuery("SELECT sqlite_version();")) {
             rs.next();
-            System.out.println(rs.getString(1));
+            return rs.getString(1);
         }
     }
 }
