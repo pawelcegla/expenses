0e52293a00ecb81c8bd9bb9b5b222e66294d7735
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-03-05 20:01:10 +0100

    #7 Add SQLite dependency

--- src/main/java/expenses/App.java
+++ src/main/java/expenses/App.java
@@ -1,12 +1,21 @@
 package expenses;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        System.out.println("Hello, I am JanB");
+        try (var c = DriverManager.getConnection("jdbc:sqlite::memory:");
+             var s = c.createStatement();
+             var rs = s.executeQuery("SELECT sqlite_version();")) {
+
+            rs.next();
+            System.out.println(rs.getString(1));
+        }
     }
 }
