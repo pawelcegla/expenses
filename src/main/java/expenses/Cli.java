3802e99bcfe22fd9e99a5613118a32be599ea4ff
Paweł Cegła <3962186+pawelcegla@users.noreply.github.com>
2023-03-24 22:37:23 +0100

    #7 Make storing expenses separate from parsing arguments

--- src/main/java/expenses/Cli.java
+++ src/main/java/expenses/Cli.java
@@ -4,30 +4,24 @@ import java.math.BigDecimal;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.joining;
 
-/**
- * Hello world!
- */
 public class Cli {
 
-    public static void main(String[] args) throws SQLException {
-        execute("jdbc:sqlite:expenses.db", "INSERT INTO expenses VALUES (?, ?, ?, ?);", s -> {
-            LocalDate.parse(args[0], ISO_LOCAL_DATE);
-            s.setString(1, args[0]);
-            s.setBigDecimal(2, new BigDecimal(args[1]));
-            s.setString(3, args[2]);
-            s.setString(4, stream(args).skip(3).map(t -> '"' + t + '"').collect(joining(",", "[", "]")));
-        });
+    public static void main(String... args) throws SQLException {
+        run("jdbc:sqlite:expenses.db", args);
     }
 
-    static void execute(String url, String sql, SetParameters set) throws SQLException {
-        try (var c = DriverManager.getConnection(url); var s = c.prepareStatement(sql)) {
-            set.apply(s);
-            s.execute();
+    static void run(String url, String... args) throws SQLException {
+        try (var c = DriverManager.getConnection(url)) {
+            var s = new Storage(c);
+            s.add(
+                    LocalDate.parse(args[0], ISO_LOCAL_DATE),
+                    new BigDecimal(args[1]),
+                    args[2],
+                    Arrays.copyOfRange(args, 3, args.length));
         }
     }
 }
---
+++ src/main/java/expenses/Storage.java
@@ -0,0 +1,29 @@
+package expenses;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+
+public class Storage {
+
+    private final Connection c;
+
+    public Storage(Connection c) {
+        this.c = c;
+    }
+
+    public void add(LocalDate date, BigDecimal amount, String description, String... tags) throws SQLException {
+        try (var s = c.prepareStatement("INSERT INTO expenses VALUES (?, ?, ?, ?);")) {
+            s.setString(1, ISO_LOCAL_DATE.format(date));
+            s.setBigDecimal(2, amount);
+            s.setString(3, description);
+            s.setString(4, stream(tags).map(t -> '"' + t + '"').collect(joining(",", "[", "]")));
+            s.executeUpdate();
+        }
+    }
+}
